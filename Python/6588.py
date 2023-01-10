3be0678fdb9e7055985af441ededdd1ad2bd4cde
cjk09083 <cjk09083@naver.com>
2023-01-10 11:31:05 +0900

    p6588

--- Python/6588.py
+++ Python/6588.py
@@ -3,11 +3,14 @@
 # https://www.acmicpc.net/problem/6588
 
 '''
-
+- 먼저 주어진 범위내의 소수를 모두 구한다.
+- a를 2부터 1씩 증가시키며 a가 소수인지 확인한다.
+- a가 소수라면 b = n-a가 소수인지 확인한다.
+- a, b가 소수라면 결과를 출력한다.
 '''
 
 if __name__ == "__main__": 
-    start, end = map(int,input().split())
+    end = 1000000
 
     fList = [0] * (end+1)
     fList[1] = 1
@@ -16,8 +19,19 @@ if __name__ == "__main__":
         if fList[i]==1:
             continue
 
-        if start  <= i <= end:
-            print(i)
         for j in range(2, (end//i)+1):
             fList[i*j] = 1
+    
+    while True:
+        n = int(input())
+        if n == 0:
+            break
+        
+        for a in range(0, end+1):
+            if fList[a]==1:
+                continue
 
+            b = n - a
+            if fList[b] == 0:
+                print(f'{n} = {a} + {b}')
+                break
