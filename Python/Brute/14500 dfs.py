71f0f37c7e4764ece5152a95487893246497557a
cjk09083 <cjk09083@naver.com>
2023-01-16 15:16:32 +0900

    p14500

---
+++ Python/Brute/14500 dfs.py
@@ -0,0 +1,48 @@
+# 2022 01 16.
+
+# https://www.acmicpc.net/problem/14500
+
+'''
+- DFS를 사용해 탐색
+- 172ms
+'''
+import sys
+input=sys.stdin.readline
+
+dx=[0,1,0,-1]
+dy=[1,0,-1,0]
+
+def dfs(x,y,depth,res):
+    global answer
+    if answer >= res + MAX*(4-L):
+        return
+    
+    if depth==4:
+        answer = max(answer, res)
+        return
+
+    for i in range(4):
+        nx=x+dx[i]
+        ny=y+dy[i]
+        if 0 <= nx < m and 0 <= ny < n and visit[ny][nx]==0:
+            if depth==2:
+                visit[ny][nx]=1
+                dfs(x,y,depth+1,res+board[ny][nx])
+                visit[ny][nx]=0
+            visit[ny][nx]=1
+            dfs(nx,ny,depth+1,res+board[ny][nx])
+            visit[ny][nx]=0
+
+if __name__ == "__main__": 
+    n,m=map(int,input().split())
+    board=[list(map(int,input().split())) for _ in range(n)]
+    answer,MAX = 0,max(map(max,board))
+    visit=[[0]*m for _ in range(n)]
+
+    for i in range(n):
+        for j in range(m):
+            visit[i][j]=1
+            dfs(j,i,1,board[i][j])
+            visit[i][j]=0
+
+    print(answer)
--- Python/Brute/14500.py
+++ Python/Brute/14500.py
@@ -1,61 +1,81 @@
-# 2022 01 14.
+# 2022 01 16.
 
 # https://www.acmicpc.net/problem/14500
 
 '''
-- closestNum를 0부터 999999까지 올려가면서 가장 가까운 숫자를 찾는다.
-- 708ms
+- 주어진 board에서 가장자리에 0배열 3칸씩 추가
+- 3,3 부터 n+3, m+3까지 변경하며 왼쪽위에서 시작하는 모든 도형의 경우의수 계산
+- 경우의 수 중 가장 큰 값을 출력
+- 1380ms
 '''
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
+def getBox():
+    board = []
+    for _ in range(3):
+        board.append([0]*(m+6))
+    for _ in range(n):
+        tmp = [0,0,0]
+        tmp += list(map(int, input().split()))
+        tmp += [0,0,0]
+        board.append(tmp)
+    for _ in range(3):
+        board.append([0]*(m+6))
 
-def isBtnOnly():
-    btnOnly = True
-    for i in str(t):
-        if i in err:
-            btnOnly = False
-            break
+    return board
 
-    return btnOnly
+def sol(i,j):
+    cnt = 0
+    # 1 직선 (세로놓기)
+    cnt = max(cnt, b[i][j] + b[i+1][j] + b[i+2][j] + b[i+3][j])
+    # 2 직선 (가로놓기)
+    cnt = max(cnt, b[i][j] + b[i][j+1] + b[i][j+2] + b[i][j+3])
+    # 3 네모
+    cnt = max(cnt, b[i][j] + b[i][j+1] + b[i+1][j+1] + b[i+1][j])
+    # 4 ㄴ // 왼상단 시작 오른 하단 종료. (세로가 큰 ㄴ)
+    cnt = max(cnt, b[i][j] + b[i+1][j] + b[i+2][j] + b[i+2][j+1])
+    # 5 ㄴ // 오른 상단 시작 왼 하단 종료. (4 좌우 대칭)
+    cnt = max(cnt, b[i][j+1] + b[i+1][j+1] + b[i+2][j+1] + b[i+2][j])
+    # 6 ㄴ // 왼하단 시작 오른 상단 종료. (가로가 긴 ㄱ)
+    cnt = max(cnt, b[i][j] + b[i][j+1] + b[i][j+2] + b[i+1][j+2])
+    # 7 ㄴ // 왼하단 시작 오른 상단 종료. (6 대칭)
+    cnt = max(cnt, b[i+1][j] + b[i][j] + b[i][j+1] + b[i][j+2])
+    # 8 ㄴ // 왼상단 시작 오른 하단 종료. (세로가 긴 ㄱ)
+    cnt = max(cnt, b[i][j] + b[i][j+1] + b[i+1][j+1] + b[i+2][j+1])
+    # 9 ㄴ // 왼상단 시작 오른 하단 종료. (8 대칭)
+    cnt = max(cnt, b[i][j+1] + b[i][j] + b[i+1][j] + b[i+2][j])
+    # 10 ㄴ // 오른 상단 시작 왼 하단 종료 (가로가 긴 ㄴ)
+    cnt = max(cnt, b[i][j] + b[i+1][j] + b[i+1][j+1] + b[i+1][j+2])
+    # 11 ㄴ // 오른 상단 시작 왼 하단 종료 (10 대칭)
+    cnt = max(cnt, b[i+1][j] + b[i+1][j+1] + b[i+1][j+2] + b[i][j+2])
+    # 12 ㄴㄱ  // 왼 상단 시작 오른 하단 종료 (ㄴ 밑에 ㄱ)
+    cnt = max(cnt, b[i][j] + b[i+1][j] + b[i+1][j+1] + b[i+2][j+1])
+    # 13 ㄴㄱ  // 왼 하단 시작 오른 상단 종료 (ㄴ 밑에 ㄱ 좌우 반전)
+    cnt = max(cnt, b[i][j+1] + b[i+1][j+1] + b[i+1][j] + b[i+2][j])
+    # 14 ㄴㄱ  // 왼 상단 시작 오른 하단 종료 (ㄱ 우측에 ㄴ)
+    cnt = max(cnt, b[i][j] + b[i][j+1] + b[i+1][j+1] + b[i+1][j+2])
+    # 15 ㄴㄱ  // 오른 상단 시작 왼 하단 종료 (ㄱ 우측에 ㄴ 상하 반전)
+    cnt = max(cnt, b[i+1][j] + b[i+1][j+1] + b[i][j+1] + b[i][j+2])
+    # 16 ㅗ // ㅜ
+    cnt = max(cnt, b[i][j] + b[i][j+1] + b[i+1][j+1] + b[i][j+2])
+    # 17 ㅗ // ㅓ
+    cnt = max(cnt, b[i][j+1] + b[i+1][j+1] + b[i+2][j+1] + b[i+1][j])
+    # 18 ㅗ // ㅗ
+    cnt = max(cnt, b[i+1][j] + b[i+1][j+1] + b[i][j+1] + b[i+1][j+2])
+    # 19 ㅗ // ㅏ
+    cnt = max(cnt, b[i][j] + b[i+1][j] + b[i+1][j+1] + b[i+2][j])
 
-def getNum():
-    if isBtnOnly():
-        return min(abs(t-100),len(str(t)))
-    
-    start = 0
-    end = 1_000_000
-    closestNum = abs(t - 100)
-
-    for i in range(start, end):
-        tmp = str(i)
-        l = len(tmp)
-        isBreak = False
-
-        for j in range(l):
-            if tmp[j] in err:
-                isBreak = True
-                break
-
-        if not isBreak:
-            c = abs(t - i) + l
-            closestNum = min(c, closestNum)
-            
-    return closestNum
+    return cnt
 
 if __name__ == "__main__": 
-    t = int(input()) 
-    n = int(input())
-    if t == 100:
-        cnt = 0
-    elif n == 0:
-        cnt = min(abs(t-100),len(str(t)))
-    elif n == 10:
-        cnt = abs(t-100)
-    else:
-        err = list(map(str, input().split()))
-        cnt = getNum()
+    n, m = map(int,(input().split()))
+    b = getBox()
+    # print(*b, sep = "\n")
+    result = 0
 
-    print(cnt)
+    for i in range(n):
+        for j in range(m):
+            result = max(result,sol(i+3,j+3))
 
-    
+    print(result)
